b04c97299c2572f5d287fedcfe4d6376fccdf1e3
Riankrn <150497135+Riankrn@users.noreply.github.com>
2023-12-30 15:48:36 +0700

    BelajarVueJs

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import "./bootstrap";
+
+import { createApp } from "vue";
+import PostsIndex from "./components/Posts/index.vue";
+
+const app = createApp({});
+app.component("posts-index", PostsIndex);
+app.mount("#app");
